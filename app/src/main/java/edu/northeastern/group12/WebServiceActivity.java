9add42c79b0a8f04e8549a317fc2fc3686ed8cc1
xialingfeng <lingfengxia.jake@gmail.com>
2023-02-19 20:01:59 -0500

    fixed some bugs, add saveInstanceState

--- app/src/main/java/edu/northeastern/group12/WebServiceActivity.java
+++ app/src/main/java/edu/northeastern/group12/WebServiceActivity.java
@@ -2,12 +2,16 @@ package edu.northeastern.group12;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -33,6 +37,7 @@ public class WebServiceActivity extends AppCompatActivity {
     Button search_openfda; //send request to web service
     private String drugName; //the name of drug users type in
     private String dosageForm; //the dosage form users type in
+    private static String[] queryResult;
     private boolean searchGeneric; //if the users are searching for generics
     Spinner spinnerDrugForm; //spinner that let users choose the dosage form
     Spinner spinnerBrandGeneric; // spinner that let users choose brand name of generic name
@@ -51,6 +56,7 @@ public class WebServiceActivity extends AppCompatActivity {
     TextView active_ingredients_tv; //text view that shows the active ingredient
     ProgressBar progressCircle; //progressive bar
     String searchQuery;
+    private final Handler textHandler = new Handler();
 
 
     @Override
@@ -74,13 +80,36 @@ public class WebServiceActivity extends AppCompatActivity {
         //initialize savedInstanceState
         init(savedInstanceState);
 
-        final String serviceURL = "https://api.fda.gov/drug/ndc.json?api_key=vXJCXZb1koUtVO6Sk2sio3X7IQHUEBYqgEjwMfKS";
-        String endpointURL = endpoint(serviceURL);
-
         search_openfda.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                sendHttpRequest(endpointURL);
+            public void onClick(View view) {
+                drugName = ((EditText) findViewById(R.id.drug_name)).getText().toString();
+                progressCircle.setVisibility(View.VISIBLE);
+                runCallThread(view);
+            }
+        });
+
+        spinnerDrugForm.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String[] drugForms = getResources().getStringArray(R.array.drugformspinner);
+                dosageForm = drugForms[position];
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        spinnerBrandGeneric.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String[] brandGeneric = getResources().getStringArray(R.array.brandgenericspinner);
+                searchBrandGeneric = brandGeneric[position];
+                searchGeneric = !searchBrandGeneric.equals("BRAND NAME");
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                searchGeneric = true;
             }
         });
 
@@ -94,6 +123,20 @@ public class WebServiceActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putString("drugName",drugName);
+        outState.putString("dosageForm",dosageForm);
+        outState.putString("searchBrandGeneric",searchBrandGeneric);
+        outState.putString("product_ndc",product_ndc);
+        outState.putString("product_type",product_type);
+        outState.putString("routes",routes);
+        outState.putString("manufacturer_name",manufacturer_name);
+        outState.putString("pharm_class_epc",pharm_class_epc);
+        outState.putString("active_ingredients",active_ingredients);
     }
 
     private void init(Bundle savedInstanceState) {
@@ -129,41 +172,64 @@ public class WebServiceActivity extends AppCompatActivity {
     }
 
 
-    private void sendHttpRequest(@NonNull String requestURL) {
-
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-
-                try {
-                    // try to do the HTTP request
-                    progressCircle.setVisibility(View.VISIBLE);
-                    URL url = new URL(requestURL);
-                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-
-                    // try to get the response as inputStream
-                    InputStream in = urlConnection.getInputStream();
-                    String result = convertStreamToString(in);
-
-                    System.out.println(result); // check response, testing only
-
-                    // TODO: process the result
-
-                    fetchData(result);
-
-                    // disconnect at end
-                    urlConnection.disconnect();
-                    progressCircle.setVisibility(View.INVISIBLE);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } catch (JSONException e) {
-                    throw new RuntimeException(e);
-                }
+    protected String[] sendHttpRequest() {
+        String[] results = new String[6];
+        URL url;
+        String ndcURL;
+        try{
+            if (searchGeneric) {
+                Log.e("searchGeneric ", "true");
+                ndcURL = "https://api.fda.gov/drug/ndc.json?search=generic_name:\"" + drugName + "\"+AND+dosage_form:\"" + dosageForm + "\"&limit=1";
+            } else{
+                Log.e("searchGeneric ", "false");
+                ndcURL = "https://api.fda.gov/drug/ndc.json?search=brand_name:\"" + drugName + "\"+AND+dosage_form:\"" + dosageForm + "\"&limit=1";
             }
-        });
+            Log.e("ndcURL",ndcURL);
+            url = new URL(ndcURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            conn.connect();
+            InputStream inputStream = conn.getInputStream();
+            final String response = convertStreamToString(inputStream);
+            JSONObject jObject = new JSONObject(response);
+            JSONObject drugResults = jObject.getJSONArray("results").getJSONObject(0);
+            product_ndc = drugResults.getString("product_ndc");
+            product_type = drugResults.getString("product_type").replace("[","").replace("]","").replace("\"","");
+            routes = drugResults.getString("route").replace("[","").replace("]","").replace("\"","");
+            JSONObject openfda = drugResults.getJSONObject("openfda");
+            manufacturer_name = openfda.getString("manufacturer_name").replace("[","").replace("]","").replace("\"","");
+            pharm_class_epc = openfda.getJSONArray("pharm_class_epc").toString().replace("[","").replace("]","").replace("\"","");
+            JSONArray active_ingredients_array = drugResults.getJSONArray("active_ingredients");
+            ArrayList<String> active_ingredients_list = new ArrayList<>();
+            for (int i = 0; i < active_ingredients_array.length(); i++) {
+                active_ingredients_list.add(active_ingredients_array.getJSONObject(i).getString("name").replace("\"",""));
+            }
+            active_ingredients = active_ingredients_list.toString().replace("[","").replace("]","").replace("\"","");
+            if (active_ingredients_array.length() > 3){
+                active_ingredients_list = new ArrayList<>();
+                active_ingredients_list.add(active_ingredients_array.getJSONObject(0).getString("name").replace("\"",""));
+                active_ingredients_list.add(active_ingredients_array.getJSONObject(1).getString("name").replace("\"",""));
+                active_ingredients_list.add(active_ingredients_array.getJSONObject(2).getString("name").replace("\"",""));
+                active_ingredients = active_ingredients_list.toString().replace("[","").replace("]","").replace("\"","");
+                active_ingredients = active_ingredients + " - (And more)";
+            }
 
-        // Start the thread
-        thread.start();
+            results[0] = product_ndc;
+            results[1] = product_type;
+            results[2] = routes;
+            results[3] = manufacturer_name;
+            results[4] = pharm_class_epc;
+            results[5] = active_ingredients;
+            Log.e("results NDC: ", "product_ndc"+ product_ndc);
+            return results;
+
+        } catch (JSONException | IOException e) {
+            e.printStackTrace();
+        }
+        results[0] = "error";
+        return results;
+
     }
 
     private static String convertStreamToString(InputStream is) throws IOException {
@@ -186,54 +252,57 @@ public class WebServiceActivity extends AppCompatActivity {
         }
     }
 
-    private String endpoint(String serviceURL) {
-        dosageForm = spinnerDrugForm.getSelectedItem().toString();
-        searchBrandGeneric = spinnerBrandGeneric.getSelectedItem().toString();
-        searchGeneric = !searchBrandGeneric.equals("BRAND NAME");
-        if (!searchGeneric) {
-            searchQuery = "patient.drug.openfda.brand_name:\"" + drugName + "\"+AND+patient.drug.openfda.dosage_form:\"" + dosageForm + "\"";
-        } else {
-            searchQuery = "patient.drug.openfda.generic_name:\"" + drugName + "\"+AND+patient.drug.openfda.dosage_form:\"" + dosageForm + "\"";
+
+    class runnableThread implements Runnable {
+        @Override
+        public void run() {
+            if (drugName != null) {
+                queryResult = sendHttpRequest();
+                boolean valid = isValidAPICall();
+                if (valid) {
+                    Log.e("openFDA", "valid api call");
+                    textHandler.post(() -> {
+                        product_ndc_tv.setText(product_ndc);
+                        product_type_tv.setText(product_type);
+                        routes_tv.setText(routes);
+                        manufacturer_name_tv.setText(manufacturer_name);
+                        pharm_class_epc_tv.setText(pharm_class_epc);
+                        active_ingredients_tv.setText(active_ingredients);
+                    });
+                } else {
+                    Log.e("openFDA", "invalid api call");
+                    textHandler.post(() -> {
+                        product_ndc_tv.setText("Invalid Search");
+                        product_type_tv.setText("Invalid Search");
+                        routes_tv.setText("Invalid Search");
+                        manufacturer_name_tv.setText("Invalid Search");
+                        pharm_class_epc_tv.setText("Invalid Search");
+                        active_ingredients_tv.setText("Invalid Search");
+                        Toast.makeText(WebServiceActivity.this, R.string.search_error, Toast.LENGTH_SHORT).show();
+                    });
+                }
+            } else {
+                textHandler.post(() -> {
+                    product_ndc_tv.setText("Invalid Search");
+                    product_type_tv.setText("Invalid Search");
+                    routes_tv.setText("Invalid Search");
+                    manufacturer_name_tv.setText("Invalid Search");
+                    pharm_class_epc_tv.setText("Invalid Search");
+                    active_ingredients_tv.setText("Invalid Search");
+                    Toast.makeText(WebServiceActivity.this, R.string.search_error, Toast.LENGTH_SHORT).show();
+                });
+            }
+            progressCircle.setVisibility(View.INVISIBLE);
         }
-        String endpointURL = serviceURL + "&search=" + searchQuery;
-        return endpointURL;
     }
 
-    private void fetchData(String json) throws JSONException {
-        if (!json.isEmpty()) {
-            JSONObject jsonObject = new JSONObject(json);
-            JSONArray jsonArray = jsonObject.getJSONArray("results");
-            JSONObject results = jsonArray.getJSONObject(0);
-            product_ndc = results.getString("product_ndc");
-            product_type = results.getString("product_type");
-            manufacturer_name = results.getJSONObject("openfda").getJSONArray("manufacturer_name").getString(0);
-            routes = results.getString("route");
-            //pharm_class_epc = results.getJSONObject("openfda").getJSONArray("pharm_class_epc").getString(0);
-            active_ingredients = results.getJSONArray("active_ingredients").get(0).toString();
+    public void runCallThread(View view){
+        runnableThread callThread = new runnableThread();
+        new Thread(callThread).start();
+    }
 
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    if (product_ndc != null) {
-                        product_ndc_tv.setText(product_ndc);
-                    }
-                    if (product_type != null) {
-                        product_type_tv.setText(product_type);
-                    }
-                    if (manufacturer_name != null) {
-                        manufacturer_name_tv.setText(manufacturer_name);
-                    }
-                    if (active_ingredients != null) {
-                        active_ingredients_tv.setText(active_ingredients);
-                    }
-                    if (pharm_class_epc != null) {
-                        pharm_class_epc_tv.setText(pharm_class_epc);
-                    }
-                    if (routes != null) {
-                        routes_tv.setText(routes);
-                    }
-                }
-            });
-        }
+    public static boolean isValidAPICall() {
+        String error = queryResult[0];
+        return error != null && !error.equals("error");
     }
 }
